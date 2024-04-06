5bf6390a5935cf05cf7de8fad9864fad642f23e7
hg <haigguol@gmail.com>
2024-04-06 14:51:28 +0800

    解析html

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"go.uber.org/zap"
+	"golang.org/x/net/html"
 	"refush-diamonds/config"
 	myZap "refush-diamonds/zap"
+	"strings"
 )
 
 func main() {
@@ -20,7 +22,9 @@ func main() {
 		panic("Unmarshal Config  Failed")
 	}
 	logger.Info("服务启动成功....")
-	//cookie := Login(config.C)
+
+	cookie := Login(config.C)
+	ShowDiamonds(cookie)
 	//var wg sync.WaitGroup
 	//loopNum := 100
 	//if config.C.ConcurrentNum > 0 {
@@ -99,7 +103,7 @@ func QianDao(cookie Cookie) {
 	// POST Map, default is JSON content type. No need to set one
 	_, err := client.R().
 		SetHeaders(map[string]string{
-			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
 			"Cookie":       ck,
 		}).
 		SetBody("action=user_qiandao").
@@ -112,4 +116,109 @@ func QianDao(cookie Cookie) {
 		return
 	}
 	fmt.Printf("status:%s,msg:%s\n", success.Status, success.Msg)
+}
+
+// ShowDiamonds 抓取当前余额
+func ShowDiamonds(cookie Cookie) {
+	client := resty.New()
+
+	type AuthSuccess struct {
+		Status string `json:"status"`
+		Msg    string `json:"msg"`
+	}
+	type AuthError struct {
+	}
+	//var success AuthSuccess
+
+	first := cookie.first
+	second := cookie.second
+	three := cookie.third
+	ck := first + second + three
+
+	// POST Map, default is JSON content type. No need to set one
+	resp, err := client.R().
+		SetHeaders(map[string]string{
+			"Content-Type": "text/html; charset=UTF-8",
+			"Cookie":       ck,
+		}).
+		// SetBody("action=user_qiandao").
+		// SetResult(&success).    // or SetResult(AuthSuccess{}).
+		SetError(&AuthError{}). // or SetError(AuthError{}).
+		Get("https://xueke58.com/user")
+	if err != nil {
+		// TODO: 不要panic,该错误直接不处理
+		fmt.Printf("err:%v\n", err)
+		return
+	}
+	//fmt.Printf("status:%s,msg:%s\n", success.Status, success.Msg)
+	// 解析 HTML 文档
+
+	doc, err := html.Parse(strings.NewReader(string(resp.Body())))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(doc.Attr)
+
+	// 查找 "现有余额" 元素
+	//balance := doc.Find(".jinbi")
+
+	// 获取文本内容
+	//text := balance.Text()
+
+	// 打印结果
+	//fmt.Println("现有余额:", text)
+
+	//fileName := "index.html"
+	//
+	//bs, err := ioutil.ReadFile(fileName)
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//text := string(bs)
+	//
+	//doc, err := html.Parse(strings.NewReader(text))
+	//
+	//if err != nil {
+	//
+	//	log.Fatal(err)
+	//}
+
+	var data []string
+
+	doTraverse(doc, &data, "span")
+	// fmt.Println(data)
+	for i, datum := range data {
+		fmt.Printf("span%d=%v\n", i, datum)
+	}
+}
+
+func doTraverse(doc *html.Node, data *[]string, tag string) {
+
+	var traverse func(n *html.Node, tag string) *html.Node
+
+	traverse = func(n *html.Node, tag string) *html.Node {
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+
+			if c.Type == html.TextNode && c.Parent.Data == tag {
+
+				*data = append(*data, c.Data)
+			}
+
+			res := traverse(c, tag)
+
+			if res != nil {
+
+				return res
+			}
+		}
+
+		return nil
+	}
+
+	traverse(doc, tag)
 }
